b5b3da58ba33c8a1856f431a2f643098cf61ce22
Joshua Robinson <joshua.fe.robinson@gmail.com>
2018-04-06 12:46:05 +0100

    Fixed python interface to work with new output files. Unit tests only operate on clusters with example geometries now.

--- test/test_clusters.py
+++ test/test_clusters.py
@@ -5,6 +5,8 @@ import pytest
 import sys, os
 sys.path.append(os.path.abspath('../lib'))
 from tcc import structures, xyz, wrapper
+from glob import glob
+structures_to_test = glob('clusters/*.xyz')
 
 def run_static_tcc_simple_bonds(x, box=[100.,100.,100.]):
     """Run TCC with pairwise cutoff bond detection algorithm.
@@ -31,34 +33,34 @@ def run_static_tcc_voronoi(x, box=[100.,100.,100.]):
     TCC = wrapper.TCCWrapper()
     return TCC.run(box,x)
 
-@pytest.mark.parametrize('cluster', structures.clusters)
-def test_simple_bonds(cluster):
+@pytest.mark.parametrize('path', structures_to_test)
+def test_simple_bonds(path):
     """Unit tests for clusters when running TCC with simple pairwise cutoff bond detection algorithm.
 
     Failure if number of detected clusters does not match known composition of given structure.
 
     Args:
-        cluster: isolated structure to run TCC
+        path: path to isolated structure geometry to run TCC on
     """
-    try: x = xyz.read('clusters/%s.xyz' % cluster)
-    except: return # temporary until we have sample coordinates for all known structures!
+    x = xyz.read(path)
+    cluster = path.split('/')[-1].split('.xyz')[0]
 
-    found = run_static_tcc_simple_bonds(x)['Number']
+    found = run_static_tcc_simple_bonds(x)['Number of clusters']
     for component,n in structures.composition[cluster].items():
         assert n == int(found[component])
 
-@pytest.mark.parametrize('cluster', structures.clusters)
-def test_voronoi(cluster):
+@pytest.mark.parametrize('path', structures_to_test)
+def test_voronoi(path):
     """Unit tests for clusters when running TCC with simple pairwise cutoff bond detection algorithm.
 
     Failure if number of detected clusters does not match known composition of given structure.
 
     Args:
-        cluster: isolated structure to run TCC
+        path: path to isolated structure geometry to run TCC on
     """
-    try: x = xyz.read('clusters/%s.xyz' % cluster)
-    except: return # temporary until we have sample coordinates for all known structures!
+    x = xyz.read(path)
+    cluster = path.split('/')[-1].split('.xyz')[0]
 
-    found = run_static_tcc_voronoi(x)['Number']
+    found = run_static_tcc_voronoi(x)['Number of clusters']
     for component,n in structures.composition[cluster].items():
         assert n == int(found[component])
