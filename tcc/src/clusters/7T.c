a501a62374790aaf1dcda39c777edbc9d7476501
Peter <merrygoat@users.noreply.github.com>
2018-04-06 21:11:43 +0100

    7T fix
    Now detecting 3 7Ts and 2 7Ta as expected for the isolated clsuters
    Id's and order of unit test cluster XYZs changed to match output of functions.

--- tcc/src/clusters/7T.c
+++ tcc/src/clusters/7T.c
@@ -2,6 +2,10 @@
 #include <tools.h>
 #include "7T.h"
 
+int get_new_particle(const int *new_5A_cluster, int spindle_id);
+
+int check_spindle_is_new_5A_spindle(int spindle_id, const int *new_5A_cluster);
+
 void Clusters_Get7T() {
     int old_6Z_id, new_5A_id;
     int *old_6Z_cluster, *new_5A_cluster;
@@ -13,17 +17,38 @@ void Clusters_Get7T() {
         old_6Z_cluster = hc6Z[old_6Z_id];
         for (int spindle_pointer = 4; spindle_pointer < 6; ++spindle_pointer) {
             spindle_id = old_6Z_cluster[spindle_pointer];
-            if (spindle_pointer == 5) new_particle_id = old_6Z_cluster[4];
-            else new_particle_id = old_6Z_cluster[5];
             for (new_5A_pointer = 0; new_5A_pointer < nmem_sp3c[spindle_id]; ++new_5A_pointer) {
                 new_5A_id = mem_sp3c[spindle_id][new_5A_pointer];
                 new_5A_cluster = hcsp3c[new_5A_id];
 
-                bond_counter = check_ring_bonds(new_5A_cluster, old_6Z_cluster);
-                check_7T_type(bond_counter, old_6Z_cluster, new_particle_id);
+                if (check_spindle_is_new_5A_spindle(spindle_id, new_5A_cluster) == 1 ) {
+                    bond_counter = check_ring_bonds(new_5A_cluster, old_6Z_cluster);
+                    if (bond_counter == 21 || bond_counter == 22 || bond_counter == 25) {
+                        new_particle_id = get_new_particle(new_5A_cluster, spindle_id);
+                        check_7T_type(bond_counter, old_6Z_cluster, new_particle_id);
+                    }
+                }
             }
         }
     }
+}
+
+int check_spindle_is_new_5A_spindle(int spindle_id, const int *new_5A_cluster) {
+    if (new_5A_cluster[3] == spindle_id) {
+        return 1;
+    }
+    else if (new_5A_cluster[4] == spindle_id) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+int get_new_particle(const int *new_5A_cluster, int spindle_id) {
+    if (new_5A_cluster[3] == spindle_id) return new_5A_cluster[4];
+    else if (new_5A_cluster[4] == spindle_id) return new_5A_cluster[3];
+    else Error("New spindle not found.");
 }
 
 int check_ring_bonds(const int *new_5A_cluster, const int *old_6Z_cluster) {
@@ -59,15 +84,15 @@ int check_ring_bonds(const int *new_5A_cluster, const int *old_6Z_cluster) {
 }
 
 void check_7T_type(int bond_counter, const int *old_6Z, int new_particle) {
-    if (bond_counter == 20) {
+    if (bond_counter == 21) {
         //detected symmetric
         add_7T_s(old_6Z, new_particle);
     }
-    if (bond_counter == 21) {
+    if (bond_counter == 22) {
         // detected assymetric type 1
         add_7T_a(old_6Z, new_particle);
     }
-    if (bond_counter == 24) {
+    if (bond_counter == 25) {
         // detected assymetric type 2
         add_7T_a(old_6Z, new_particle);
     }
@@ -78,8 +103,7 @@ void add_7T_a(const int *old_6Z, int new_particle) {
 
     if (n7T_a == m7T_a) {
         hc7T_a = resize_2D_int(hc7T_a, m7T_a, m7T_a + incrStatic, clusSize, -1);
-        m7T_a = m7T_a
-                + incrStatic;
+        m7T_a = m7T_a + incrStatic;
     }
 
     hc7T_a[n7T_a][0] = old_6Z[0];
