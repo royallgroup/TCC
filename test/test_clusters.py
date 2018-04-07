526e400b1a00280c06d4560b9506e5bea4c9ec9c
Peter <merrygoat@users.noreply.github.com>
2018-04-07 18:22:51 +0100

    Modified unit tests

--- test/test_clusters.py
+++ test/test_clusters.py
@@ -1,17 +1,17 @@
 """Unit tests for detecting individual clusters."""
 
 import pytest
-
-import sys, os
-sys.path += [os.path.abspath('../lib')]
-
+import sys
+import os
 import numpy, pandas
 from glob import glob
-
 from tcc import structures, xyz, wrapper
-structures_to_test = glob('clusters/*.xyz')
 
-def run_static_tcc_simple_bonds(x, box=[100.,100.,100.]):
+structures_to_test = glob('clusters/*.xyz')
+sys.path += [os.path.abspath('../lib')]
+
+
+def run_static_tcc_simple_bonds(x, box=(100., 100., 100.)):
     """Run TCC with pairwise cutoff bond detection algorithm.
 
     Args:
@@ -24,7 +24,8 @@ def run_static_tcc_simple_bonds(x, box=[100.,100.,100.]):
     TCC.input_parameters['Simulation']['bond_type'] = wrapper.BondType.simple
     return TCC.run(box,x)
 
-def run_static_tcc_voronoi(x, box=[100.,100.,100.]):
+
+def run_static_tcc_voronoi(x, box=(100., 100., 100.)):
     """Run TCC with voronoi bond detection algorithm.
 
     Args:
@@ -36,22 +37,26 @@ def run_static_tcc_voronoi(x, box=[100.,100.,100.]):
     TCC = wrapper.TCCWrapper()
     return TCC.run(box,x)
 
-@pytest.mark.parametrize('path', structures_to_test)
-def test_simple_bonds(path):
-    """Unit tests for clusters when running TCC with simple pairwise cutoff bond detection algorithm.
+
+def run_test(path, bond_type):
+    """Unit tests for clusters when running TCC.
 
     Failure if number of detected clusters does not match known composition of given structure.
 
     Args:
         path: path to isolated structure geometry to run TCC on
+        bond_type: the type of bond to be used by the TCC
     """
-    x = xyz.read(path)
-    cluster = path.split('/')[-1].split('.xyz')[0]
+    particle_coordinates = xyz.read(path)
+    cluster_name = path.split('/')[-1].split('.xyz')[0]
 
-    found = run_static_tcc_simple_bonds(x)['Number of clusters']
-    report = pandas.DataFrame(found)
+    if bond_type == "voronoi":
+        tcc_result_report = run_static_tcc_voronoi(particle_coordinates)['Number of clusters']
+    else:
+        tcc_result_report = run_static_tcc_simple_bonds(particle_coordinates)['Number of clusters']
+    report = pandas.DataFrame(tcc_result_report)
     report['Expected'] = 0
-    for component,n in structures.composition[cluster].items():
+    for component, n in structures.composition[cluster_name].items():
         report['Expected'][component] = n
 
     try:
@@ -60,26 +65,8 @@ def test_simple_bonds(path):
         print(report)
         raise AssertionError from None
 
+
 @pytest.mark.parametrize('path', structures_to_test)
-def test_voronoi(path):
-    """Unit tests for clusters when running TCC with simple pairwise cutoff bond detection algorithm.
-
-    Failure if number of detected clusters does not match known composition of given structure.
-
-    Args:
-        path: path to isolated structure geometry to run TCC on
-    """
-    x = xyz.read(path)
-    cluster = path.split('/')[-1].split('.xyz')[0]
-
-    found = run_static_tcc_simple_bonds(x)['Number of clusters']
-    report = pandas.DataFrame(found)
-    report['Expected'] = 0
-    for component,n in structures.composition[cluster].items():
-        report['Expected'][component] = n
-
-    try:
-        assert numpy.all(report['Number of clusters'] == report['Expected'])
-    except AssertionError:
-        print(report)
-        raise AssertionError from None
+def test_clusters(path):
+    run_test(path, "simple")
+    run_test(path, "voronoi")
