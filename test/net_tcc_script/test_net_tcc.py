9ecb9cb23aca2919ed892a8b51e2c3edc02bdd36
Peter <merrygoat@users.noreply.github.com>
2018-04-09 02:27:27 +0100

    Updated net clusters to take priority as argument

--- test/net_tcc_script/test_net_tcc.py
+++ test/net_tcc_script/test_net_tcc.py
@@ -1,14 +1,18 @@
 import subprocess
 from glob import glob
-import shutil
 import filecmp
 import os
+import sys
 
+sys.path += [os.path.abspath('../../lib/net_clusters')]
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+from net import net_cluster_calculation
 
 class cd:
     """Context manager for changing the current working directory"""
-    def __init__(self, newPath):
-        self.newPath = os.path.expanduser(newPath)
+    def __init__(self, new_path):
+        self.newPath = os.path.expanduser(new_path)
 
     def __enter__(self):
         self.savedPath = os.getcwd()
@@ -20,19 +24,10 @@ class cd:
 
 class FileOperations:
     @staticmethod
-    def copy_net():
-        # Copy the exectuable to the current directory
-        try:
-            shutil.copy("../../../lib/net_clusters/net.py", os.getcwd())
-            return 0
-        except Exception as e:
-            print(e)
-            return 1
-
-    @staticmethod
     def run_net():
         try:
-            return subprocess.call(["python", "net.py", "sample.xyz"])
+            net_cluster_calculation("./raw_output",  "(FCC, 13A, 12E, 11F, 10B, 9B, 8B, sp5c, sp4c, sp3c)")
+            return 0
         except Exception as e:
             print(e)
             return 1
@@ -40,21 +35,18 @@ class FileOperations:
     @staticmethod
     def tidy():
         # Remove the files we have created
-        os.remove("net.py")
-        for file in (glob("sample.xyz*")):
-            os.remove(file)
+        os.remove("./raw_output/net_clusters.txt")
         return 0
 
 
 class FileChecks:
     @staticmethod
     def check_net():
-        return filecmp.cmp("sample_net.txt", glob("sample.xyz*net.txt")[0], shallow=False)
+        return filecmp.cmp("sample_net.txt", "./raw_output/net_clusters.txt", shallow=False)
 
 
 def test_basic_configuration():
     with cd("./basic_configuration"):
-        assert FileOperations.copy_net() == 0
         assert FileOperations.run_net() == 0
         assert FileChecks.check_net() is True
         assert FileOperations.tidy() == 0
