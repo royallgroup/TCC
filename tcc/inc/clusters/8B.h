4cf1f1e0fcc57743e2261da40283aa2f69d42d4f
Peter <merrygoat@users.noreply.github.com>
2018-04-12 13:02:14 +0100

    8B refactor
    tests passing.

--- tcc/inc/clusters/8B.h
+++ tcc/inc/clusters/8B.h
@@ -3,7 +3,7 @@
 
 void Clusters_Get8B();
 
-void Clusters_8B_loop(int i, int clusSize, int s1, int s2);
+void Clusters_8B_loop(int first_7A_id, int s1, int s2);
 
 void Cluster_Write_8B();
 
--- tcc/src/clusters/8B.c
+++ tcc/src/clusters/8B.c
@@ -3,70 +3,91 @@
 #include "bonds.h"
 #include "tools.h"
 
-void Clusters_Get8B() { // Detect 8B Cs clusters
-    int i;
-    int clusSize=8;
+int is_particle_in_7A(int first_7A_id, int new_particle_id);
 
-    for (i=0; i<nsp5c; ++i) {    // loop over all 7A_i
-        Clusters_8B_loop(i, clusSize, hcsp5c[i][5], hcsp5c[i][6]);
-        Clusters_8B_loop(i, clusSize, hcsp5c[i][6], hcsp5c[i][5]);
+void Clusters_Get8B() { // Detect 8B Cs clusters
+    int first_7A_id;
+
+
+    for (first_7A_id = 0; first_7A_id < nsp5c; ++first_7A_id) {    // loop over all 7A_i
+        Clusters_8B_loop(first_7A_id, hcsp5c[first_7A_id][5], hcsp5c[first_7A_id][6]);
+        Clusters_8B_loop(first_7A_id, hcsp5c[first_7A_id][6], hcsp5c[first_7A_id][5]);
     }
 }
 
-void Clusters_8B_loop(int i, int clusSize, int primary_spindle, int secondary_spindle) {
+void Clusters_8B_loop(int first_7A_id, int primary_spindle, int secondary_spindle) {
 
-    int j, k, l, m;
-    int n1, nbs, unc[3];
+    int new_particle_pointer, k, l, m;
+    int new_particle_id, nbs, unc[3];
     int break_out;
+    int clusSize = 8;
 
-    for (j=0; j < num_bonds[primary_spindle]; ++j) { // loop over all j particles bonded to first spindle of 7A_i
-        n1 = bNums[primary_spindle][j];
-        for (k=0; k<5; ++k) if (n1 == hcsp5c[i][k]) break;
-        if (k<5) continue;
-        if (n1 == secondary_spindle) continue; // now is n1 bonded to sp5
-        nbs = 0; // number of bonds
-        for (k=0; k<5; ++k) if (Bonds_BondCheck(n1, hcsp5c[i][k])) ++nbs;
-        if (nbs != 2) continue;
+    for (new_particle_pointer = 0; new_particle_pointer < num_bonds[primary_spindle]; ++new_particle_pointer) {
+        new_particle_id = bNums[primary_spindle][new_particle_pointer];
 
-        // Now we have found the 8B Cs cluster
-        if (n8B==m8B) {
-            hc8B=resize_2D_int(hc8B,m8B,m8B+incrStatic,clusSize,-1);
-            m8B=m8B+incrStatic;
-        }
-        l=0;
-        m=3;
-        break_out=0;
-        for (k=0; k<5; ++k) {
-            if (Bonds_BondCheck(n1, hcsp5c[i][k])) {
-                if (m==5) {
-                    break_out=1;
-                    break;
+        if(is_particle_in_7A(first_7A_id, new_particle_id) == 0) {
+
+            if (new_particle_id == secondary_spindle) continue; // now is new_particle_id bonded to sp5
+
+            nbs = 0; // number of bonds
+            for (k = 0; k < 5; ++k) {
+                if (Bonds_BondCheck(new_particle_id, hcsp5c[first_7A_id][k])) {
+                    ++nbs;
                 }
-                hc8B[n8B][m]=hcsp5c[i][k];
-                m++;
             }
-            else {
-                if (l==3) {
-                    break_out=1;
-                    break;
+            if (nbs != 2) continue;
+
+            // Now we have found the 8B Cs cluster
+            if (n8B == m8B) {
+                hc8B = resize_2D_int(hc8B, m8B, m8B + incrStatic, clusSize, -1);
+                m8B = m8B + incrStatic;
+            }
+
+            l = 0;
+            m = 3;
+            break_out = 0;
+            for (k = 0; k < 5; ++k) {
+                if (Bonds_BondCheck(new_particle_id, hcsp5c[first_7A_id][k])) {
+                    if (m == 5) {
+                        break_out = 1;
+                        break;
+                    }
+                    hc8B[n8B][m] = hcsp5c[first_7A_id][k];
+                    m++;
+                } else {
+                    if (l == 3) {
+                        break_out = 1;
+                        break;
+                    }
+                    unc[l] = hcsp5c[first_7A_id][k];
+                    l++;
                 }
-                unc[l]=hcsp5c[i][k];
-                l++;
             }
+            if (break_out == 1 || m < 5 || l < 3) continue;
+
+            quickSort(&hc8B[n8B][3], 2);
+            for (k = 0; k < 3; k++) hc8B[n8B][k] = unc[k];
+            quickSort(&hc8B[n8B][0], 3);
+
+            hc8B[n8B][5] = secondary_spindle;
+            hc8B[n8B][6] = primary_spindle;
+            hc8B[n8B][7] = new_particle_id;
+
+
+            Cluster_Write_8B();
         }
-        if (break_out==1 || m<5 || l<3) continue;
-
-        quickSort(&hc8B[n8B][3],2);
-        for (k=0;k<3;k++) hc8B[n8B][k]=unc[k];
-        quickSort(&hc8B[n8B][0],3);
-
-        hc8B[n8B][5]=secondary_spindle;
-        hc8B[n8B][6]=primary_spindle;
-        hc8B[n8B][7]=n1;
-
-
-        Cluster_Write_8B();
     }
+}
+
+int is_particle_in_7A(int first_7A_id, int new_particle_id) {
+    int is_in_7A = 0;
+    for (int first_7A_pointer = 0; first_7A_pointer < 5; ++first_7A_pointer) {
+        if (new_particle_id == hcsp5c[first_7A_id][first_7A_pointer]) {
+            is_in_7A = 1;
+            break;
+        }
+    }
+    return is_in_7A;
 }
 
 void Cluster_Write_8B() {
