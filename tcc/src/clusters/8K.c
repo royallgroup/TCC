695da61388b8026abd9fdcd2f9483abb19fc41b3
Peter <merrygoat@users.noreply.github.com>
2018-04-12 15:06:49 +0100

    8K refactor
    Tests passing

--- tcc/src/clusters/8K.c
+++ tcc/src/clusters/8K.c
@@ -1,28 +1,26 @@
-#include <clusters/simple_cluster_methods.h>
+#include "simple_cluster_methods.h"
 #include "8K.h"
 #include "globals.h"
 #include "tools.h"
 
 int is_particle_in_5A_ring(const int *second_5A_cluster, int first_5A_ring_particle, int *cp);
 
-void Clusters_Get8K() {    // Detect 8K clusters
-    int first_5A_id, second_5A_pointer, first_5A_ring_pointer, k, l, m, n;
-    int second_5A_id;
-    int *first_5A_cluster, *second_5A_cluster;
-    int first_5A_ring_particle;
+void Clusters_Get8K() {
+    int k, l, m, n;
     int cp[2], unc[3], scom, sother[2];
     int clusSize = 8;
 
     cp[0] = cp[1] = unc[0] = unc[1] = unc[2] = scom = sother[0] = sother[1] = -1;
 
-    for (first_5A_id = 0; first_5A_id < nsp3c; ++first_5A_id) {
-        first_5A_cluster = hcsp3c[first_5A_id];
-        for (first_5A_ring_pointer = 0; first_5A_ring_pointer < 3; first_5A_ring_pointer++) {
-            first_5A_ring_particle = first_5A_cluster[first_5A_ring_pointer];
-            for (second_5A_pointer = 0; second_5A_pointer < nmem_sp3c[first_5A_ring_particle]; ++second_5A_pointer) {
-                second_5A_id = mem_sp3c[first_5A_ring_particle][second_5A_pointer];
+    for (int first_5A_id = 0; first_5A_id < nsp3c; ++first_5A_id) {
+        int *first_5A_cluster = hcsp3c[first_5A_id];
+        for (int first_5A_ring_pointer = 0; first_5A_ring_pointer < 3; first_5A_ring_pointer++) {
+            int first_5A_ring_particle = first_5A_cluster[first_5A_ring_pointer];
+            for (int second_5A_pointer = 0;
+                 second_5A_pointer < nmem_sp3c[first_5A_ring_particle]; ++second_5A_pointer) {
+                int second_5A_id = mem_sp3c[first_5A_ring_particle][second_5A_pointer];
                 if (second_5A_id <= first_5A_id) continue;
-                second_5A_cluster = hcsp3c[second_5A_id];
+                int *second_5A_cluster = hcsp3c[second_5A_id];
 
                 if (is_particle_in_5A_ring(second_5A_cluster, first_5A_ring_particle, cp) == 1) {
 
@@ -43,10 +41,16 @@ void Clusters_Get8K() {    // Detect 8K clusters
                         }
                         if (m != 1) continue;
 
-                        if (first_5A_cluster[3] == scom) sother[0] = first_5A_cluster[4];
-                        else sother[0] = first_5A_cluster[3];
-                        if (second_5A_cluster[3] == scom) sother[1] = second_5A_cluster[4];
-                        else sother[1] = second_5A_cluster[3];
+                        if (first_5A_cluster[3] == scom) {
+                            sother[0] = first_5A_cluster[4];
+                        } else {
+                            sother[0] = first_5A_cluster[3];
+                        }
+                        if (second_5A_cluster[3] == scom) {
+                            sother[1] = second_5A_cluster[4];
+                        } else {
+                            sother[1] = second_5A_cluster[3];
+                        }
 
                         k = 0;    // find particle from SP3 ring of sp3c_i which is common with 5A mem_sp3c[sp3c[i][j2]][j]
                         for (l = 0; l < 3; ++l) {
@@ -63,93 +67,97 @@ void Clusters_Get8K() {    // Detect 8K clusters
                                 k++;
                             }
                         }
-                        if (k != 1) continue;
+                        if (k != 1) {
+                            continue;
+                        }
 
                         k = 0;    // find particle from SP3 ring of mem_sp3c[sp3c[i][j2]][j] which is common with 5A sp3c_i
                         for (l = 0; l < 3; ++l) {
-                            if (second_5A_cluster[l] == cp[0] || second_5A_cluster[l] == cp[1]) continue;
-                            for (m = 0; m < 5; ++m) {
-                                if (second_5A_cluster[l] == first_5A_cluster[m]) break;
-                            }
-                            if (m == 5) {
-                                if (k == 1) {
-                                    k++;
-                                    break;
-                                }
-                                unc[1] = second_5A_cluster[l];
-                                k++;
-                            }
-                        }
-                        if (k != 1) continue;
-
-                        for (k = second_5A_pointer + 1; k < nmem_sp3c[first_5A_ring_particle]; ++k) {
-                            if (mem_sp3c[first_5A_ring_particle][k] <= first_5A_id)
-                                continue;  // higher index of sp3c cluster than i
-                            if (mem_sp3c[first_5A_ring_particle][k] <= second_5A_id)
-                                continue;   // higher index of sp3c cluster than mem_sp3c[sp3c[i][j2]][j]
-
-                            n = 0;  // check common SP3 ring particles are exactly cp
-                            for (l = 0; l < 3; ++l) {
-                                for (m = 0; m < 2; ++m) {
-                                    if (cp[m] == hcsp3c[mem_sp3c[first_5A_ring_particle][k]][l]) {
-                                        n++;
-                                    }
-                                }
-                            }
-                            if (n != 2) continue;
-
-                            n = 0;  // check spindles are exactly sother
-                            for (l = 3; l < 5; ++l) {
-                                for (m = 0; m < 2; ++m) {
-                                    if (sother[m] == hcsp3c[mem_sp3c[first_5A_ring_particle][k]][l]) {
-                                        n++;
-                                    }
-                                }
-                            }
-                            if (n != 2) continue;
-
-                            n = 0;    // find particle from SP3 ring of mem_sp3c[sp3c[i][j2]][j] which is common with 5A sp3c_i
-                            for (l = 0; l < 3; ++l) {
-                                if (hcsp3c[mem_sp3c[first_5A_ring_particle][k]][l] == cp[0] ||
-                                    hcsp3c[mem_sp3c[first_5A_ring_particle][k]][l] == cp[1])
-                                    continue;
+                            if (second_5A_cluster[l] != cp[0] && second_5A_cluster[l] != cp[1]) {
                                 for (m = 0; m < 5; ++m) {
-                                    if (hcsp3c[mem_sp3c[first_5A_ring_particle][k]][l] == first_5A_cluster[m] ||
-                                        hcsp3c[mem_sp3c[first_5A_ring_particle][k]][l] ==
-                                        second_5A_cluster[l])
-                                        break;
+                                    if (second_5A_cluster[l] == first_5A_cluster[m]) break;
                                 }
                                 if (m == 5) {
-                                    if (n == 1) {
-                                        n++;
+                                    if (k == 1) {
+                                        k++;
                                         break;
                                     }
-                                    unc[2] = hcsp3c[mem_sp3c[first_5A_ring_particle][k]][l];
-                                    n++;
+                                    unc[1] = second_5A_cluster[l];
+                                    k++;
                                 }
                             }
-                            if (n != 1) continue;
+                        }
+                        if (k != 1) {
+                            continue;
+                        }
 
-                            // Now we have found the 8K cluster
-                            if (n8K == m8K) {
-                                hc8K = resize_2D_int(hc8K, m8K, m8K + incrStatic, clusSize, -1);
-                                m8K = m8K + incrStatic;
+                        for (int third_5A_pointer = second_5A_pointer + 1; third_5A_pointer < nmem_sp3c[first_5A_ring_particle]; ++third_5A_pointer) {
+                            int third_5A_id = mem_sp3c[first_5A_ring_particle][third_5A_pointer];
+                            if (third_5A_id > first_5A_id && third_5A_id > second_5A_id) {
+                                int *third_5A_cluster = hcsp3c[third_5A_id];
+                                n = 0;  // check common SP3 ring particles are exactly cp
+                                for (l = 0; l < 3; ++l) {
+                                    for (m = 0; m < 2; ++m) {
+                                        if (cp[m] == third_5A_cluster[l]) {
+                                            n++;
+                                        }
+                                    }
+                                }
+                                if (n != 2) continue;
+
+                                n = 0;  // check spindles are exactly sother
+                                for (l = 3; l < 5; ++l) {
+                                    for (m = 0; m < 2; ++m) {
+                                        if (sother[m] == third_5A_cluster[l]) {
+                                            n++;
+                                        }
+                                    }
+                                }
+                                if (n != 2) continue;
+
+                                n = 0;    // find particle from SP3 ring of mem_sp3c[sp3c[i][j2]][j] which is common with 5A sp3c_i
+                                for (l = 0; l < 3; ++l) {
+                                    if (third_5A_cluster[l] == cp[0] ||
+                                        third_5A_cluster[l] == cp[1])
+                                        continue;
+                                    for (m = 0; m < 5; ++m) {
+                                        if (third_5A_cluster[l] == first_5A_cluster[m] ||
+                                            third_5A_cluster[l] ==
+                                            second_5A_cluster[l])
+                                            break;
+                                    }
+                                    if (m == 5) {
+                                        if (n == 1) {
+                                            n++;
+                                            break;
+                                        }
+                                        unc[2] = third_5A_cluster[l];
+                                        n++;
+                                    }
+                                }
+                                if (n != 1) continue;
+
+                                // Now we have found the 8K cluster
+                                if (n8K == m8K) {
+                                    hc8K = resize_2D_int(hc8K, m8K, m8K + incrStatic, clusSize, -1);
+                                    m8K = m8K + incrStatic;
+                                }
+                                // hc8K key: (SP3_common_1, SP3_common_2, spindle_1, spindle_2, spindle_3, other_SP3_1, other_SP3_2, other_SP3_3)
+                                hc8K[n8K][0] = cp[0];
+                                hc8K[n8K][1] = cp[1];
+                                hc8K[n8K][2] = scom;
+                                hc8K[n8K][3] = sother[0];
+                                hc8K[n8K][4] = sother[1];
+                                hc8K[n8K][5] = unc[0];
+                                hc8K[n8K][6] = unc[1];
+                                hc8K[n8K][7] = unc[2];
+
+                                quickSort(&hc8K[n8K][0], 2);
+                                quickSort(&hc8K[n8K][2], 3);
+                                quickSort(&hc8K[n8K][5], 3);
+
+                                Cluster_Write_8K();
                             }
-                            // hc8K key: (SP3_common_1, SP3_common_2, spindle_1, spindle_2, spindle_3, other_SP3_1, other_SP3_2, other_SP3_3)
-                            hc8K[n8K][0] = cp[0];
-                            hc8K[n8K][1] = cp[1];
-                            hc8K[n8K][2] = scom;
-                            hc8K[n8K][3] = sother[0];
-                            hc8K[n8K][4] = sother[1];
-                            hc8K[n8K][5] = unc[0];
-                            hc8K[n8K][6] = unc[1];
-                            hc8K[n8K][7] = unc[2];
-
-                            quickSort(&hc8K[n8K][0], 2);
-                            quickSort(&hc8K[n8K][2], 3);
-                            quickSort(&hc8K[n8K][5], 3);
-
-                            Cluster_Write_8K();
                         }
                     }
                 }
