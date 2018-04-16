cd1f897224d3b357755ec3f810cd6f9f61623735
Joshua Robinson <joshua.fe.robinson@gmail.com>
2018-04-16 14:43:29 +0100

    Unified snapshot interface to configuration files and finished documentation.

---
+++ lib/tcc/snapshot.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+"""
+Module defining interface for reading and writing snapshots in various file formats.
+
+The module defines:
+  - stream_safe_open: a context manager to facilitate parsing both open file streams and paths with the same interface
+  - NoSnapshotError: exception raised when a snapshot could not be read from the file
+  - snapshot: the class containing the general snapshot interface
+"""
+
+import sys, numpy, contextlib
+
+@contextlib.contextmanager
+def stream_safe_open(path_or_file, mode='r'):
+    """Context manager for parsers which accept an open file stream or a file path to open.
+
+    Args:
+        path_or_file: either an open file stream (in which case the context manager does nothing and returns it) or a path (in which case the context manager will open this, return a stream and then clean up)
+        mode: mode to open file in
+    """
+    if isinstance(path_or_file, str):
+        f = file_to_close = open(path_or_file, mode)
+    else:
+        f = path_or_file
+        file_to_close = None
+
+    try:
+        yield f
+    finally:
+        if file_to_close:
+            file_to_close.close()
+
+class NoSnapshotError(RuntimeError):
+    """Exception raised when not able to read a snapshot from a file."""
+
+class Snapshot:
+    """Snapshot of a system of particles.
+
+    Variables:
+        n: number of particles
+        d: dimensionality of configuration space
+        x: particle coordinates (n by d container)
+        box: box containing the particles (d by 2 container)
+        species: labels of the particle species (string or container of strings)
+        time: time or frame of the snapshot within a trajectory
+    """
+
+    def __init__(self, x=numpy.empty((0,0)), box=None, species=None, time=0):
+        """Create a new snapshot.
+
+        Args:
+            x: particle coordinates
+            box: box dimensions containing the particles
+            species: labels of the particle species
+            time: time or frame of the snapshot within a trajectory
+        """
+        self.x = x
+        self.box = box
+        self.species = species
+        self.time = time
+
+    def copy(self):
+        """Return a deep copy of the snapshot."""
+        return snapshot(self.x.copy(), self.box.copy(), self.species.copy(), self.time)
+
+    @property
+    def n(self):
+        """Number of particles in snapshot."""
+        return len(self.x)
+
+    @property
+    def d(self):
+        """Dimensionality of configuration space."""
+        return self.x.shape[1]
+
+    @classmethod
+    def read_single(cls, path_or_file):
+        """Read a single snapshot from the disk.
+
+        Example:
+        >>> snapshot.read('snapshot.atom')
+        <snapshot n=10976 t=0>
+
+        Args:
+            cls: derived class defining specific file format
+            path_or_file: file stream or path to read snapshot from
+        Returns:
+            snapshot: the snapshot read from disk
+        Raises:
+            NoSnapshotError: if could not read from file
+            RuntimeException: if did not recognise file format
+        """
+        with stream_safe_open(path_or_file) as f:
+            snap = cls()
+            snap.read(f)
+            return snap
+
+    @classmethod
+    def read_trajectory(cls, path_or_file, max_frames=None):
+        """Read a trajectory (i.e. multiple snapshots) from the disk.
+
+        Example where trajectory.atom is an atom file containing two snapshots:
+        >>> list(read('trajectory.atom', 2))
+        [<snapshot n=10976 t=0>, <snapshot n=10976 t=1>]
+
+        Args:
+            cls: derived class defining specific file format
+            path_or_file: file stream or path to read trajectory from
+        Returns:
+            trajectory (generator): generator iterating through the snapshots in the trajectory
+        Raises:
+            NoSnapshotError: if could not read from file
+            RuntimeException: if did not recognise file format
+        """
+        with stream_safe_open(path_or_file) as f:
+            frames = 0
+            while True:
+                try: snap = cls.read_single(f)
+                except NoSnapshotError: break
+
+                yield snap
+                frames += 1
+                if max_frames is not None and frames is max_frames: break
+
+    def write(self, out=sys.stdout):
+        """Dump the snapshot to a file in LAMMPS (.atom) format.
+
+        Args:
+            out: file or path to write the snapshot to
+        """
+        with stream_safe_open(out, 'w') as f:
+            f.write(str(self))
+            f.write('\n')
+
+    def __repr__(self):
+        """Internal representation of the object for printing to debugger."""
+        return '<snapshot n=%r t=%r>' % (self.n, self.time)
+
+    def __str__(self):
+        """String representation of the snapshot in the chosen format not implemented in the base class, and must be written for specific formats."""
+        raise NotImplementedError
--- lib/tcc/xyz.py
+++ lib/tcc/xyz.py
@@ -1,53 +1,75 @@
 #!/usr/bin/env python3
-import sys, os, io, pandas
-import numpy as np
+"""
+Module for reading and writing snapshots from and to XYZ (.xyz) file formats. The main class is XYZSnapshot, but some additional functions are defined to provide a simplified interface to this class.
 
-def write(xyz, out=sys.stdout, comment='', atoms='A'):
-    if type(out) is str: out = open(out, 'w')
-    if type(xyz) is not list: xyz = [xyz]
+The module defines:
+  - XYZSnapshot: the class the defining the file interface to this file format
+  - read: shorthand for XYZSnapshot.read_single
+  - read_trajectory: shorthand for XYZSnapshot.read_trajectory
+"""
 
-    for x in xyz:
-        out.write(str(len(x)) + '\n')
-        out.write(comment + '\n')
-        if type(atoms) is str:
-            for i in range(len(x)):
-                out.write(atoms + ' ' + ' '.join(map(str, x[i,:])) + '\n')
+import sys, io, numpy, pandas
+from .snapshot import stream_safe_open, NoSnapshotError, Snapshot
+
+class XYZSnapshot(Snapshot):
+    """Snapshot of a system of particles in XYZ (.xyz) file format.
+
+    Interface defined in parent class Snapshot. Further documentation can be found there.
+    """
+
+    def read(self, path_or_file):
+        """Read a snapshot from a file, overwriting any existing data.
+
+        Args:
+            path_or_file: file stream or path to read snapshot from
+        Raises:
+            NoSnapshotError: if could not read from file
+        """
+        with stream_safe_open(path_or_file) as f:
+            # Read the header - check for EOF.
+            line = f.readline()
+            if not line: raise NoSnapshotError
+
+            # Process the rest of the header rows.
+            number_of_atoms = int(line)
+            if not number_of_atoms > 0: raise NoSnapshotError
+            comment = f.readline()
+
+            # Use pandas to read the main table.
+            c = io.StringIO()
+            for i in range(number_of_atoms): c.write(f.readline())
+            c.seek(0)
+            table = pandas.read_table(c, sep='\s+', names=('atom','x','y','z'), nrows=number_of_atoms)
+
+            self.x = table[['x','y','z']].values.copy('c').astype(numpy.longdouble)
+            self.species = table['atom'].tolist()
+            self.time = self.box = None
+
+    def __str__(self):
+        """String representation of the snapshot in XYZ (.xyz) format"""
+        f = io.StringIO()
+
+        # Header states number of particles (we have ignored comment line)
+        f.write('%d\n' % self.n)
+
+        # Single component system
+        if type(self.species) is str:
+            for i in range(self.n):
+                f.write('\n')
+                f.write('%s ' % self.species)
+                f.write(' '.join(map(str, self.x[i,:])))
+        # Handle particle species separately
         else:
-            for i in range(len(x)):
-                out.write(atoms[i] + ' ' + ' '.join(map(str, x[i,:])) + '\n')
+            for i in range(self.n):
+                f.write('\n')
+                f.write('%s ' % self.species[i])
+                f.write(' '.join(map(str, self.x[i,:])))
 
-def read(stream, read_atoms=False):
-    if type(stream) is str: stream = open(stream)
+        return f.getvalue()
 
-    # Read the header - check for EOF.
-    line = stream.readline()
-    if not line: return None
-    # Process the rest of the header rows.
-    number_of_atoms = int(line)
-    assert number_of_atoms is not None and number_of_atoms > 0
-    comment = stream.readline()
-
-    # Use pandas to read the main table.
-    c = io.StringIO()
-    for i in range(number_of_atoms): c.write(stream.readline())
-    c.seek(0)
-    table = pandas.read_table(c, sep='\s+', names=('atom','x','y','z'), nrows=number_of_atoms)
-
-    coordinates = table[['x','y','z']].values.copy('c').astype(np.longdouble)
-
-    if read_atoms: return table['atom'].tolist(), coordinates
-    else: return coordinates
-
-def multi_read(path, max_frames=None, read_atoms=False):
-    with open(path) as f:
-        frames = 0
-        while True:
-            configuration = read(f, read_atoms)
-            if configuration is None: break
-
-            yield configuration
-            frames += 1
-            if max_frames is not None and frames is max_frames: break
-
-def trajectory(path, max_frames=None, read_atoms=False):
-    return list(multi_read(path, max_frames, read_atoms))
+def read(*args, **kwargs):
+    """Read a single snapshot from the disk."""
+    return XYZSnapshot.read_single(*args, **kwargs)
+def read_trajectory(*args, **kwargs):
+    """Read a trajectory (i.e. multiple snapshots) from the disk."""
+    return XYZSnapshot.read_trajectory(*args, **kwargs)
