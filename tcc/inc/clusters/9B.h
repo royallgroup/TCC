ddc36d1326bdf4a8ea9680d19b2555d912d4bbb3
Peter <merrygoat@users.noreply.github.com>
2018-05-04 15:50:36 +0100

    9B refactor complete
    Tests passing.

--- tcc/inc/clusters/9B.h
+++ tcc/inc/clusters/9B.h
@@ -3,6 +3,7 @@
 
 void Clusters_Get9B_10B_11B_11E_12D();
 
-void Cluster_Write_9B();
+void Cluster_Write_9B(const int* uncommon_ring_particles, const int *common_ring_particles,
+                      const int *uncommon_spindles, int common_spindle_id);
 
 #endif
