7fd8f90d4bc9a07b62f1f59ef3e0764f1741bb04
Peter <merrygoat@users.noreply.github.com>
2018-05-05 22:50:05 +0100

    Revert "10A refactor"
    This reverts commit 0b7353622d296c15c749027cd9ab02da05950786.

--- tcc/inc/clusters/10A.h
+++ tcc/inc/clusters/10A.h
@@ -3,6 +3,6 @@
 
 void Clusters_Get10A();
 
-void Cluster_Write_10A(const int *first_sp4b_cluster, const int *second_sp4b_cluster);
+void Cluster_Write_10A();
 
 #endif
--- tcc/src/clusters/10A.c
+++ tcc/src/clusters/10A.c
@@ -13,76 +13,79 @@ void Clusters_Get10A() {
    *      - The spindles are not bonded.
    *      - Each particle in each sp4 ring is bonded to exactly two particles in the other sp4 ring
    *
-   *  Cluster output: BBBBBBBOO
-   *  Storage order: first_cluster_ring x 4, second_cluster_ring x 4, first_cluster_spindle, second_cluster_spindle
+   *  Cluster output: BBBBBBOOO
+   *  Storage order: ring particles x 8, spindles x 2
    */
 
-    int k, l, m;
+    int i, j, j2, k, l, m;
+    int clusSize=10;
+    int *used_sp4b;
 
-    for (int first_sp4b_id = 0; first_sp4b_id < nsp4b; ++first_sp4b_id) {
-        int *first_sp4b_cluster = hcsp4b[first_sp4b_id];
-        for (int neighbour_pointer = 0; neighbour_pointer < num_bonds[first_sp4b_cluster[0]]; ++neighbour_pointer) {
-            for (int second_sp4b_pointer = 0; second_sp4b_pointer < nmem_sp4b[bNums[first_sp4b_cluster[0]][neighbour_pointer]]; ++second_sp4b_pointer) {
-                int second_sp4b_id = mem_sp4b[bNums[first_sp4b_cluster[0]][neighbour_pointer]][second_sp4b_pointer];
-                int *second_sp4b_cluster = hcsp4b[second_sp4b_id];
-                if (second_sp4b_id > first_sp4b_id) {
+    used_sp4b = malloc(nsp4b * sizeof(int));
+    if (used_sp4b == NULL) { 
+        Error("Clusters_Get10A(): used_sp4b[] malloc out of memory\n");
+    }
+    for (i=0; i < nsp4b; ++i) {
+        used_sp4b[i] = 0;
+    }
 
-                    if (first_sp4b_cluster[4] != second_sp4b_cluster[4]) {
-                        if (Bonds_BondCheck(first_sp4b_cluster[4], second_sp4b_cluster[4]) == 0) {
+    for (i=0; i < nsp4b - 1; ++i) {  // loop over all sp4b_i
+        for (j2=0; j2 < nsp4b; ++j2) used_sp4b[j2] = 0;
+        used_sp4b[i]=1;
+        for (j2=0; j2 < num_bonds[hcsp4b[i][0]]; ++j2) {
+            for (j=0; j < nmem_sp4b[bNums[hcsp4b[i][0]][j2]]; ++j) {    // loop over sp4b_j
+                if (mem_sp4b[bNums[hcsp4b[i][0]][j2]][j] <= i) continue;
+                if (used_sp4b[mem_sp4b[bNums[hcsp4b[i][0]][j2]][j]] == 1) continue;
+                used_sp4b[mem_sp4b[bNums[hcsp4b[i][0]][j2]][j]]=1;
 
-                            // Check that there are no shared particles between two clusters
-                            for (k = 0; k < 5; ++k) {
-                                for (l = 0; l < 5; ++l) {
-                                    if (first_sp4b_cluster[k] == second_sp4b_cluster[l]) break;
-                                }
-                                if (l < 5) break;
-                            }
-                            if (k < 5) continue;
-
-                            // Check there are two bonds to each ring particle
-                            for (k = 0; k < 4; ++k) {
-                                m = 0;
-                                for (l = 0; l < 4; ++l)
-                                    if (Bonds_BondCheck(first_sp4b_cluster[k], second_sp4b_cluster[l]))++m;
-                                if (m != 2) break;
-                            }
-
-                            // ERROR: Need to check converse, i. e. each SP4 ring particle from sp4b_j bonded to to exactly two particles from sp4b_i
-                            if (k == 4) {
-
-
-                                Cluster_Write_10A(first_sp4b_cluster, second_sp4b_cluster);
-
-                            }
-                        }
+                if(hcsp4b[i][4] == hcsp4b[mem_sp4b[bNums[hcsp4b[i][0]][j2]][j]][4]) continue;
+                if(Bonds_BondCheck(hcsp4b[i][4], hcsp4b[mem_sp4b[bNums[hcsp4b[i][0]][j2]][j]][4])) continue;
+                for(k=0; k<5; ++k){
+                    for(l=0; l<5; ++l){
+                        if(hcsp4b[i][k] == hcsp4b[mem_sp4b[bNums[hcsp4b[i][0]][j2]][j]][l]) break;
                     }
+                    if(l<5) break;
+                }
+                if(k<5) continue;
+                for(k=0; k<4; ++k){
+                    m = 0;
+                    for(l=0; l<4; ++l) if(Bonds_BondCheck(hcsp4b[i][k], hcsp4b[mem_sp4b[bNums[hcsp4b[i][0]][j2]][j]][l])) ++m;
+                    if(m!=2) break;
+                }
+                // ERROR: Need to check converse, i. e. each SP4 ring particle from sp4b_j bonded to to exactly two particles from sp4b_i
+                if(k==4) {
+                    if(n10A == m10A) {
+                        hc10A= resize_2D_int(hc10A, m10A, m10A + incrStatic, clusSize, -1);
+                        m10A= m10A + incrStatic;
+                    }
+
+                    // hc10A key: (SP4s going up, spindles going up)
+
+                    hc10A[n10A][0]= hcsp4b[i][0];
+                    hc10A[n10A][1]= hcsp4b[i][1];
+                    hc10A[n10A][2]= hcsp4b[i][2];
+                    hc10A[n10A][3]= hcsp4b[i][3];
+                    hc10A[n10A][4]= hcsp4b[mem_sp4b[bNums[hcsp4b[i][0]][j2]][j]][0];
+                    hc10A[n10A][5]= hcsp4b[mem_sp4b[bNums[hcsp4b[i][0]][j2]][j]][1];
+                    hc10A[n10A][6]= hcsp4b[mem_sp4b[bNums[hcsp4b[i][0]][j2]][j]][2];
+                    hc10A[n10A][7]= hcsp4b[mem_sp4b[bNums[hcsp4b[i][0]][j2]][j]][3];
+                    quickSort(&hc10A[n10A][0], 8);
+                    hc10A[n10A][8]= hcsp4b[i][4];
+                    hc10A[n10A][9]= hcsp4b[mem_sp4b[bNums[hcsp4b[i][0]][j2]][j]][4];
+                    quickSort(&hc10A[n10A][8], 2);
+
+                    Cluster_Write_10A();
                 }
             }
         }
     }
+    free(used_sp4b);
 }
 
-void Cluster_Write_10A(const int *first_sp4b_cluster, const int *second_sp4b_cluster) {
-    int clusSize = 10;
+void Cluster_Write_10A() {
+    int i;
 
-    if (n10A == m10A) {
-        hc10A = resize_2D_int(hc10A, m10A, m10A + incrStatic, clusSize, -1);
-        m10A = m10A + incrStatic;
-    }
-
-    hc10A[n10A][0] = first_sp4b_cluster[0];
-    hc10A[n10A][1] = first_sp4b_cluster[1];
-    hc10A[n10A][2] = first_sp4b_cluster[2];
-    hc10A[n10A][3] = first_sp4b_cluster[3];
-    hc10A[n10A][4] = second_sp4b_cluster[0];
-    hc10A[n10A][5] = second_sp4b_cluster[1];
-    hc10A[n10A][6] = second_sp4b_cluster[2];
-    hc10A[n10A][7] = second_sp4b_cluster[3];
-    hc10A[n10A][8] = first_sp4b_cluster[4];
-    hc10A[n10A][9] = second_sp4b_cluster[4];
-
-
-    for(int i = 0; i < 8; i++) {
+    for(i=0; i<8; i++) {
         if (s10A[hc10A[n10A][i]] == 'C') s10A[hc10A[n10A][i]] = 'B';
     }
     s10A[hc10A[n10A][8]] = 'O';
