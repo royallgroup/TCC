56c35ba93e1bc813aae3e8289177ab6ec5b59c3c
Joshua Robinson <joshua.fe.robinson@gmail.com>
2018-05-10 16:42:02 +0100

    Additive hard spheres will now be parsed from the DynamO square well formats completing #65. If the square well describes any other system, the parser will raise an error.

--- lib/tcc/dynamo.py
+++ lib/tcc/dynamo.py
@@ -14,6 +14,8 @@ from .snapshot import stream_safe_open, NoSnapshotError, Snapshot
 from lxml import etree as ElementTree
 from lxml.etree import Element
 
+class NonadditiveError(RuntimeError): pass
+
 class DynamoSnapshot(Snapshot):
     """Snapshot of a system of particles in DynamO (.xml) file format.
 
@@ -24,6 +26,100 @@ class DynamoSnapshot(Snapshot):
         """Standard interface for reading trajectories should throw error because dynamo trajectories are not stored in the usual simple format."""
         raise NotImplementedError
 
+    def is_hard_sphere(self, interaction):
+        """Determine whether an interaction encoded in the dynamo XML tree is a hard sphere
+        interaction or not.
+
+        There are two ways to encode hard spheres: either explicitly as a hard sphere, or as
+        a square well with zero well depth (although the latter should be avoided as it results
+        in a less efficient simulation). This helper function tests for both eventualities.
+
+        Args:
+            interaction: xml entry for the interaction
+        Returns:
+            boolean stating whether the interaction is a hard sphere
+        """
+        if interaction.attrib['Type'] == 'HardSphere': return True
+        return interaction.attrib['Type'] == 'SquareWell' and interaction.attrib['WellDepth'] == '0'
+
+    def assign_diameters(self, uij_range, diameter):
+        """Assign diameters to the particles across the given ID range.
+
+        Diameters will only be assigned if the range describes a single-species interaction.
+        Any other range is potentially a nonadditive interaction so an exception is raised as
+        a warning.
+
+        Args:
+            uij_range: xml entry describing the range of the interaction
+            diameter: length scale of these hard spheres
+        Raises:
+            NonadditiveError: if the given range is a cross-species interaction, so potentially
+                              the dynamo file describes a nonadditive potential.
+        """
+        range_type = uij_range.attrib['Type']
+
+        if range_type == 'All':
+            self.diameters[:] = diameter
+
+        elif range_type  == 'Single':
+            uij_range = uij_range.getchildren()[0].attrib
+            start = int(uij_range['Start'])
+            end = int(uij_range['End'])
+            self.diameters[start:end+1] = diameter
+
+        elif range_type == 'Pair':
+            range0,range1 = uij_range.getchildren()
+            start0,start1 = [int(r.attrib['Start']) for r in [range0,range1]]
+            end0,end1 = [int(r.attrib['End']) for r in [range0,range1]]
+
+            if start0 == start1 and end0 == end1:
+                assigning_diameters = self.diameters[start0:end0+1]
+                if numpy.all(numpy.isnan(assigning_diameters)):
+                    assigning_diameters[:] = diameter
+                    return
+                else:
+                    raise RuntimeError('can only process additive hard spheres!')
+            else:
+                raise NonadditiveError
+
+        else:
+            raise RuntimeError('unknown range type encounted: %s' % range_type)
+
+    def verify_cross_species_interactions(self, uij_range, diameter, eps=1e-12):
+        """Check whether a cross-species hard sphere interaction is consistent with the known
+        diameters of each species, assuming additive interactions.
+
+        Additive cross-species interactions have size \sigma = 0.5*(\sigma_i + \sigma_j) where
+        \sigma_{i,j} are the diameters of the individual species. If the diameter does not
+        match this expectation then the interaction is not additive.
+
+        Args:
+            uij_range: xml entry describing the range of the interaction
+            diameter: length scale of this cross-species hard sphere interaction
+            eps: machine tolerance for the test of additivity
+        Raises:
+            NonadditiveError: if the interaction is not additive
+        """
+
+        if uij_range.attrib['Type'] != 'Pair': return
+
+        range0,range1 = uij_range.getchildren()
+        start0,start1 = [int(r.attrib['Start']) for r in [range0,range1]]
+        end0,end1 = [int(r.attrib['End']) for r in [range0,range1]]
+
+        if start0 == start1 and end0 == end1: return
+
+        diameters0 = self.diameters[start0:end0+1]
+        diameters1 = self.diameters[start1:end1+1]
+        if numpy.all(diameters0 == diameters0[0]) and numpy.all(diameters1 == diameters1[0]):
+            diameters0 = diameters0[0]
+            diameters1 = diameters1[0]
+            additive_diameter = 0.5*(diameters0 + diameters1)
+
+            if abs(additive_diameter - diameter) < eps: return
+
+        raise NonadditiveError('cross interaction between ranges (%d,%d) and (%d,%d) is non additive!' % (start0,end0,start1,end1))
+
     def read(self, path_or_file):
         """Read a snapshot from a file, overwriting any existing data.
 
@@ -31,7 +127,8 @@ class DynamoSnapshot(Snapshot):
             path_or_file: file stream or path to read snapshot from
         Raises:
             NoSnapshotError: if could not read from file
-            RuntimeException: if did not recognise file format
+            RuntimeException: if did not recognise file format or the data does not describe an
+                              additive hard sphere system
         """
         with stream_safe_open(path_or_file) as f:
             parser = ElementTree.XMLParser(remove_blank_text=True)
@@ -65,25 +162,36 @@ class DynamoSnapshot(Snapshot):
             self.volume = numpy.product(box_lengths)
             self.density = self.n / self.volume
 
-            # Find the diameters of the particles, assuming additive interactions.
-            self.diameters = numpy.empty(self.n)
+            ## Find the diameters of the particles, assuming additive interactions.
+
+            # Initialise diameters to NaN: if any are still NaN at the end we know some were
+            # uninitialised and the data file is incomplete
+            self.diameters = numpy.full(self.n, numpy.nan)
+
+            # Assign diameters
+            definitely_additive = True
             for uij in self.xml['interactions']:
-                if uij.attrib['Type'] != 'HardSphere':
-                    raise RuntimeException('I can only read additive hard spheres in this version!')
+                if not self.is_hard_sphere(uij):
+                    raise RuntimeError('can only process additive hard spheres!')
 
                 uij_range = uij.find('IDPairRange')
-                if uij_range.attrib['Type'] == 'Pair':
-                    raise RuntimeException('I can only read additive hard spheres in this version!')
-                if uij_range.attrib['Type'] == 'All':
-                    self.diameters[:] = float(uij.attrib['Diameter'])
-                    break
+                diameter = float(uij.attrib['Diameter'])
+                try: self.assign_diameters(uij_range, diameter)
+                except NonadditiveError: definitely_additive = False
 
-                uij_range = uij_range.getchildren()[0].attrib
-                start = int(uij_range['Start'])
-                end = int(uij_range['End'])
-                self.diameters[start:end+1] = float(uij.attrib['Diameter'])
+            # Check that all diameters were assigned above
+            uninitialised = numpy.isnan(self.diameters)
+            if numpy.any(uninitialised):
+                raise RuntimeError('some diameters were uninitialised! indices=%r' %
+                                   numpy.where(uninitialised))
 
-            # Compute exact volume fraction.
+            if not definitely_additive:
+                for uij in self.xml['interactions']:
+                    uij_range = uij.find('IDPairRange')
+                    diameter = float(uij.attrib['Diameter'])
+                    self.verify_cross_species_interactions(uij_range, diameter)
+
+            ## Compute exact volume fraction.
             intrinsic_volumes = numpy.pi * self.diameters**3 / 6
             self.volume_fraction = numpy.sum(intrinsic_volumes) / self.volume
 
