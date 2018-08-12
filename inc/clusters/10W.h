a595469763b5adc7b7a36243029ac57b8e1a40ea
Peter <merrygoat@users.noreply.github.com>
2018-08-12 04:07:00 +0100

    10W refactor complete
    Tests passing.

--- inc/clusters/10W.h
+++ inc/clusters/10W.h
@@ -3,6 +3,8 @@
 
 void Clusters_Get10W();
 
+int count_shared_sp5bs(int *neighbouring_sp5_ids, int first_sp5b_id, int center_id);
+
 int get_shell_particle_ids(int *shell_ids, const int *neighbouring_sp5_ids);
 
 void Cluster_Write_10W(int center_id, int *shell_ids);
--- src/clusters/10W.c
+++ src/clusters/10W.c
@@ -1,3 +1,4 @@
+#include <clusters/simple_cluster_methods.h>
 #include "10W.h"
 #include "globals.h"
 #include "tools.h"
@@ -18,29 +19,17 @@ void Clusters_Get10W() {
 
     int shell_ids[9], neighbouring_sp5_ids[5];
 
-
     for (int first_sp5b_id = 0; first_sp5b_id < nsp5b; ++first_sp5b_id) {
         int *first_sp5b_cluster = hcsp5b[first_sp5b_id];
         int center_id = first_sp5b_cluster[5];  // The id of the shared spindle at the center of the 10W
         if (num_bonds[center_id] == 9) {   // central particle must have coordination number 9
 
-            int num_shared_sp5b = 0;    // find 5 other sp5b's with spindle in common with sp5b_i
-            for (int other_sp5b_pointer = 0; other_sp5b_pointer < nmem_sp5b[center_id]; ++other_sp5b_pointer) {
-                int other_sp5_id = mem_sp5b[center_id][other_sp5b_pointer];
-                if (other_sp5_id > first_sp5b_id) {
-                    if (num_shared_sp5b < 9) {
-                        neighbouring_sp5_ids[num_shared_sp5b] = other_sp5_id;
-                    }
-                    num_shared_sp5b++;
-                }
-            }
-            if (num_shared_sp5b == 5) {
+            if (count_shared_sp5bs(neighbouring_sp5_ids, first_sp5b_id, center_id) == 5) {
                 for (int i = 0; i < 5; i++) {
                     shell_ids[i] = first_sp5b_cluster[i];
                 }
 
-                int num_shell_particles = get_shell_particle_ids(shell_ids, neighbouring_sp5_ids);
-                if (num_shell_particles == 9) {
+                if (get_shell_particle_ids(shell_ids, neighbouring_sp5_ids)) {
                     Cluster_Write_10W(center_id, shell_ids);
                 }
             }
@@ -48,29 +37,44 @@ void Clusters_Get10W() {
     }
 }
 
+int count_shared_sp5bs(int *neighbouring_sp5_ids, const int first_sp5b_id, const int center_id) {
+    // Count how many sp5b's have a spindle in common with with spindle in common with first_sp5b_id and get their ids
+    int num_shared_sp5b = 0;
+    for (int other_sp5b_pointer = 0; other_sp5b_pointer < nmem_sp5b[center_id]; ++other_sp5b_pointer) {
+        int other_sp5_id = mem_sp5b[center_id][other_sp5b_pointer];
+        if (other_sp5_id > first_sp5b_id) {
+            if (num_shared_sp5b < 9) {
+                neighbouring_sp5_ids[num_shared_sp5b] = other_sp5_id;
+            }
+            num_shared_sp5b++;
+        }
+    }
+    return num_shared_sp5b;
+}
+
 int get_shell_particle_ids(int *shell_ids, const int *neighbouring_sp5_ids) {
+    // Once we have a 10W, get the ids of the particles in the 10W that are not in the first sp5b
+    // Return 1 if there are 9 shell particles (valid 10W), else return 0.
     int num_shell_particles = 5;
     for (int neighbouring_sp5_pointer = 0; neighbouring_sp5_pointer < 5; neighbouring_sp5_pointer++) {
         int *neighbouring_cluster = hcsp5b[neighbouring_sp5_ids[neighbouring_sp5_pointer]];
-        for (int k = 0; k < 5; k++) {
-            int shell_pointer;
-            for (shell_pointer = 0; shell_pointer < num_shell_particles; shell_pointer++) {
-                if (shell_ids[shell_pointer] == neighbouring_cluster[k]) {
-                    break;
-                }
-            }
-            if (shell_pointer == num_shell_particles) {
+        for (int i = 0; i < 5; i++) {
+            if (is_particle_in_cluster(shell_ids, num_shell_particles, neighbouring_cluster[i]) == 0) {
                 if (num_shell_particles >= 9) {
-                    num_shell_particles++;
-                    break;
+                    return 0;
                 }
-                shell_ids[num_shell_particles] = neighbouring_cluster[k];
+                shell_ids[num_shell_particles] = neighbouring_cluster[i];
                 num_shell_particles++;
             }
         }
-        if (num_shell_particles >= 10) break;
     }
-    return num_shell_particles;
+
+    if (num_shell_particles == 9) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
 }
 
 void Cluster_Write_10W(int center_id, int *shell_ids) {
