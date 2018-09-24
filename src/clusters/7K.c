54b6c0c1bf3d49a07cc6a58f09a0d15881c63504
Peter <merrygoat@users.noreply.github.com>
2018-09-24 18:17:57 +0100

    Added debug script for 7K

--- src/clusters/7K.c
+++ src/clusters/7K.c
@@ -18,18 +18,14 @@ void Clusters_Get7K() {
     *  Storage order: common spindle, other spindle x 2, common ring x 2, other ring x 2)
     */
 
-    int first_5A_id, first_5A_spindle_pointer;
-    int second_5A_id, second_5A_pointer;
-    int *first_5A_cluster, *second_5A_cluster;
     int common_spindle_id[2], other_spindle_ids[2], common_ring_ids[5], uncommon_ring_particles[2];
 
-    for (first_5A_id = 0; first_5A_id < nsp3c; ++first_5A_id) {  // loop over all 5A_i
-        first_5A_cluster = hcsp3c[first_5A_id];
-        for (first_5A_spindle_pointer = 3;
-             first_5A_spindle_pointer < 5; ++first_5A_spindle_pointer) {    // loop over both spindles of 5A_i
-            for (second_5A_pointer = 0; second_5A_pointer < nmem_sp3c[first_5A_cluster[first_5A_spindle_pointer]]; ++second_5A_pointer) {  // loop over all 5A_j common with spindle of 5A_i
-                second_5A_id = mem_sp3c[first_5A_cluster[first_5A_spindle_pointer]][second_5A_pointer];
-                second_5A_cluster = hcsp3c[second_5A_id];
+    for (int first_5A_id = 0; first_5A_id < nsp3c; ++first_5A_id) {
+        int *first_5A_cluster = hcsp3c[first_5A_id];
+        for (int first_5A_spindle_pointer = 3; first_5A_spindle_pointer < 5; ++first_5A_spindle_pointer) {
+            for (int second_5A_pointer = 0; second_5A_pointer < nmem_sp3c[first_5A_cluster[first_5A_spindle_pointer]]; ++second_5A_pointer) {  // loop over all 5A_j common with spindle of 5A_i
+                int second_5A_id = mem_sp3c[first_5A_cluster[first_5A_spindle_pointer]][second_5A_pointer];
+                int *second_5A_cluster = hcsp3c[second_5A_id];
                 if (second_5A_id > first_5A_id) {
                     // exactly one common spindle between 5A_i and 5A_j
                     if (count_common_spindle_particles(first_5A_cluster, second_5A_cluster, 5, 5, common_spindle_id) == 1) {
@@ -55,24 +51,22 @@ void Clusters_Get7K() {
     }
 }
 
-void Cluster_Write_7K(int scom, int *sother, int *common_ring_particles, int *uncommon_ring_particles) {
+void Cluster_Write_7K(int common_spindle_id, int *other_spindle_ids, int *common_ring_particles, int *uncommon_ring_particles) {
     int clusSize = 7;
 
     if (n7K == m7K) {
         hc7K = resize_2D_int(hc7K, m7K, m7K + incrStatic, clusSize, -1);
         m7K = m7K + incrStatic;
     }
-    // Now we have found the 7K cluster
 
-    hc7K[n7K][0] = scom;
-    hc7K[n7K][1] = sother[0];
-    hc7K[n7K][2] = sother[1];
+    hc7K[n7K][0] = common_spindle_id;
+    hc7K[n7K][1] = other_spindle_ids[0];
+    hc7K[n7K][2] = other_spindle_ids[1];
     hc7K[n7K][3] = common_ring_particles[0];
     hc7K[n7K][4] = common_ring_particles[1];
     hc7K[n7K][5] = uncommon_ring_particles[0];
     hc7K[n7K][6] = uncommon_ring_particles[1];
 
-    // hc7K key: (scom, sother, ring_com, ring_other)
     s7K[hc7K[n7K][0]] = 'O';
     s7K[hc7K[n7K][1]] = 'O';
     s7K[hc7K[n7K][2]] = 'O';
