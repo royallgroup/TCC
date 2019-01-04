ae2c22c9381795832999609df4960a1af17ce34e
Peter <13117069+merrygoat@users.noreply.github.com>
2019-01-04 15:34:50 +0000

    10B refactor
    Tackling just the top loop first

--- src/clusters/10B.c
+++ src/clusters/10B.c
@@ -1,6 +1,7 @@
 #include <globals.h>
 #include <bonds.h>
 #include <tools.h>
+#include <clusters/simple_cluster_methods.h>
 #include "10B.h"
 
 //!  An 10B cluster is the intersection of a 9B and a 7A cluster.
@@ -17,136 +18,66 @@
 *  Storage order: ordered_shell_particles x 6, spindles x 3, common_spindle
 */
 void Clusters_Get10B(int j) {        // Return 1 if 9B is also 10B cluster
-    int k,l,m;
+    int l,m;
     int flg1, flg2;
     int trial[10];
     int break_out;
     int clusSize=10;
 
-    for (k=j+1; k < nsp5c; ++k) {  // loop over all 7A_k
-        if (hcsp5c[k][5] == hc9B[n9B][8]) {    // check one spindle of 7A_k is the common spindle of 9B (hc9B[id9B][.] at this point)
-            if (Bonds_BondCheck(hcsp5c[k][6], hc9B[n9B][6]) == 1) {  // check other spindle of 7A_k is bonded to spindle d1 of 9B
-                if (Bonds_BondCheck(hcsp5c[k][6], hc9B[n9B][7]) == 1) {  // check other spindle of 7A_k is bonded to spindle d2 of 9B
+    for (int second_7A = j + 1; second_7A < nsp5c; ++second_7A) {  // loop over all 7A_k
+        int *second_7A_cluster = hcsp5c[second_7A];
+        int *parent_9B = hc9B[n9B];
+        // At least one 7A spindle must be the common spindle of 9B
+        if (second_7A_cluster[5] == parent_9B[8]) {
+            // The parent 9B has two spindle particles
+            int first_9B_spindle = parent_9B[6];
+            int second_9B_spindle = parent_9B[7];
 
-                    flg1 = 0;
-                    flg2 = 0;
-                    for (l = 0; l < 5; l++) {
-                        if (hcsp5c[k][l] == hc9B[n9B][6]) {
-                            flg1 = 1;
-                            continue;
-                        }
-                        if (hcsp5c[k][l] == hc9B[n9B][7]) {
-                            flg2 = 1;
-                            continue;
-                        }
-                    }
-                    if (flg1 == 0 || flg2 == 0) continue;
-                    trial[6] = hc9B[n9B][6];
-                    trial[7] = hc9B[n9B][7];
-                    trial[8] = hcsp5c[k][6];
-                    trial[9] = hc9B[n9B][8];
+            // Check the 9B spindles are bonded to the 7A spindle.
+            if (Bonds_BondCheck(second_7A_cluster[6], first_9B_spindle) == 0) continue;
+            if (Bonds_BondCheck(second_7A_cluster[6], second_9B_spindle) == 0) continue;
 
-                    m = 0;
-                    break_out = 0;
-                    for (l = 0; l < 6; l++) {
-                        if (hc9B[n9B][l] == hcsp5c[k][6]) continue;
-                        if (m == 5) {
-                            m++;
-                            break_out = 1;
-                            break;
-                        }
-                        trial[m] = hc9B[n9B][l];
-                        m++;
-                    }
-                    if (break_out == 1 || m != 5) continue;
+            // Check that both of the 9B spindles are in the 7A ring
+            if (is_particle_in_cluster(second_7A_cluster, 5, first_9B_spindle) == 0) continue;
+            if (is_particle_in_cluster(second_7A_cluster, 5, second_9B_spindle) == 0) continue;
 
-                    break_out = 0;
-                    for (l = 0; l < 5; l++) {
-                        if (hcsp5c[k][l] == hc9B[n9B][6]) continue;
-                        if (hcsp5c[k][l] == hc9B[n9B][7]) continue;
-                        for (m = 0; m < 5; m++) {
-                            if (hcsp5c[k][l] == trial[m]) break;
-                        }
-                        if (m == 5) {
-                            trial[5] = hcsp5c[k][l];
-                            break_out++;
-                        }
-                    }
-                    if (break_out != 1) continue;
+            // Build the trial cluster
+            trial[6] = first_9B_spindle;
+            trial[7] = second_9B_spindle;
+            trial[8] = second_7A_cluster[6];
+            trial[9] = parent_9B[8];
 
-                    if (n10B == m10B) {
-                        hc10B = resize_2D_int(hc10B, m10B, m10B + incrStatic, clusSize, -1);
-                        m10B = m10B + incrStatic;
-                    }
-                    // Now we have found the 10B C3v cluster
-                    // ###### NOTE #####
-                    // we have sterically assumed that
-                    // 1) one member of the SP5 ring of 7A_k is common with one member of SP5 ring of 7A_i
-                    // (this member of 7A_i was uncommon to the SP5 ring of 7A_j)
-                    // 2) one member of the SP5 ring of 7A_k is common with one member of SP5 ring of 7A_j
-                    // (this member of 7A_j was uncommon to the SP5 ring of 7A_i)
-
-                    quickSort(&trial[0], 6);
-                    quickSort(&trial[6], 3);
-                    for (l = 0; l < 10; l++) hc10B[n10B][l] = trial[l];
-
-                    Cluster_Write_10B();
-                }
-            }
-        }
-
-        if (hcsp5c[k][6] == hc9B[n9B][8]) {    // check one spindle of 7A_k is the common spindle of 9B (hc9B[id9B][.] at this point)
-            if (Bonds_BondCheck(hcsp5c[k][5], hc9B[n9B][6]) == 0) continue;  // check other spindle of 7A_k is bonded to spindle d1 of 9B
-            if (Bonds_BondCheck(hcsp5c[k][5], hc9B[n9B][7]) == 0) continue;  // check other spindle of 7A_k is bonded to spindle d2 of 9B
-
-            flg1=0;
-            flg2=0;
-            for (l=0;l<5;l++) {
-                if (hcsp5c[k][l] == hc9B[n9B][6]) {
-                    flg1=1;
-                    continue;
-                }
-                if (hcsp5c[k][l] == hc9B[n9B][7]) {
-                    flg2=1;
-                    continue;
-                }
-            }
-            if (flg1==0 || flg2==0) continue;
-            trial[6]= hc9B[n9B][6];
-            trial[7]= hc9B[n9B][7];
-            trial[8]= hcsp5c[k][5];
-            trial[9]= hc9B[n9B][8];
-
-            m=0;
-            break_out=0;
-            for (l=0;l<6;l++) {
-                if (hc9B[n9B][l] == hcsp5c[k][5]) continue;
-                if (m==5) {
+            m = 0;
+            break_out = 0;
+            for (l = 0; l < 6; l++) {
+                if (parent_9B[l] == second_7A_cluster[6]) continue;
+                if (m == 5) {
                     m++;
-                    break_out=1;
+                    break_out = 1;
                     break;
                 }
-                trial[m]= hc9B[n9B][l];
+                trial[m] = parent_9B[l];
                 m++;
             }
-            if (break_out==1 || m!=5) continue;
+            if (break_out == 1 || m != 5) continue;
 
-            break_out=0;
-            for (l=0;l<5;l++) {
-                if (hcsp5c[k][l] == hc9B[n9B][6]) continue;
-                if (hcsp5c[k][l] == hc9B[n9B][7]) continue;
-                for (m=0;m<5;m++) {
-                    if (hcsp5c[k][l] == trial[m]) break;
+            break_out = 0;
+            for (l = 0; l < 5; l++) {
+                if (second_7A_cluster[l] == first_9B_spindle) continue;
+                if (second_7A_cluster[l] == second_9B_spindle) continue;
+                for (m = 0; m < 5; m++) {
+                    if (second_7A_cluster[l] == trial[m]) break;
                 }
-                if (m==5) {
-                    trial[5]= hcsp5c[k][l];
+                if (m == 5) {
+                    trial[5] = second_7A_cluster[l];
                     break_out++;
                 }
             }
-            if (break_out!=1) continue;
+            if (break_out != 1) continue;
 
-            if (n10B == m10B) { hc10B= resize_2D_int(hc10B, m10B, m10B + incrStatic, clusSize, -1);
-                m10B= m10B + incrStatic;
+            if (n10B == m10B) {
+                hc10B = resize_2D_int(hc10B, m10B, m10B + incrStatic, clusSize, -1);
+                m10B = m10B + incrStatic;
             }
             // Now we have found the 10B C3v cluster
             // ###### NOTE #####
@@ -156,24 +87,90 @@ void Clusters_Get10B(int j) {        // Return 1 if 9B is also 10B cluster
             // 2) one member of the SP5 ring of 7A_k is common with one member of SP5 ring of 7A_j
             // (this member of 7A_j was uncommon to the SP5 ring of 7A_i)
 
-            quickSort(&trial[0],6);
-            quickSort(&trial[6],3);
-            for (l=0;l<10;l++) hc10B[n10B][l]=trial[l];
+            quickSort(&trial[0], 6);
+            quickSort(&trial[6], 3);
+            for (l = 0; l < 10; l++) hc10B[n10B][l] = trial[l];
 
-            // hc10B key: (ordered shell particles, s1, s2, s3 (ordered), s_com)
+            Cluster_Write_10B();
+        }
+
+        if (second_7A_cluster[6] == parent_9B[8]) {    // check one spindle of 7A_k is the common spindle of 9B (hc9B[id9B][.] at this point)
+            if (Bonds_BondCheck(second_7A_cluster[5], parent_9B[6]) == 0) continue;  // check other spindle of 7A_k is bonded to spindle d1 of 9B
+            if (Bonds_BondCheck(second_7A_cluster[5], parent_9B[7]) == 0) continue;  // check other spindle of 7A_k is bonded to spindle d2 of 9B
+
+            flg1 = 0;
+            flg2 = 0;
+            for (l = 0; l < 5; l++) {
+                if (second_7A_cluster[l] == parent_9B[6]) {
+                    flg1 = 1;
+                    continue;
+                }
+                if (second_7A_cluster[l] == parent_9B[7]) {
+                    flg2 = 1;
+                    continue;
+                }
+            }
+            if (flg1 == 0 || flg2 == 0) continue;
+            trial[6] = parent_9B[6];
+            trial[7] = parent_9B[7];
+            trial[8] = second_7A_cluster[5];
+            trial[9] = parent_9B[8];
+
+            m = 0;
+            break_out = 0;
+            for (l = 0; l < 6; l++) {
+                if (parent_9B[l] == second_7A_cluster[5]) continue;
+                if (m == 5) {
+                    m++;
+                    break_out = 1;
+                    break;
+                }
+                trial[m] = parent_9B[l];
+                m++;
+            }
+            if (break_out == 1 || m != 5) continue;
+
+            break_out = 0;
+            for (l = 0; l < 5; l++) {
+                if (second_7A_cluster[l] == parent_9B[6]) continue;
+                if (second_7A_cluster[l] == parent_9B[7]) continue;
+                for (m = 0; m < 5; m++) {
+                    if (second_7A_cluster[l] == trial[m]) break;
+                }
+                if (m == 5) {
+                    trial[5] = second_7A_cluster[l];
+                    break_out++;
+                }
+            }
+            if (break_out != 1) continue;
+
+            if (n10B == m10B) {
+                hc10B = resize_2D_int(hc10B, m10B, m10B + incrStatic, clusSize, -1);
+                m10B = m10B + incrStatic;
+            }
+            // Now we have found the 10B C3v cluster
+            // ###### NOTE #####
+            // we have sterically assumed that
+            // 1) one member of the SP5 ring of 7A_k is common with one member of SP5 ring of 7A_i
+            // (this member of 7A_i was uncommon to the SP5 ring of 7A_j)
+            // 2) one member of the SP5 ring of 7A_k is common with one member of SP5 ring of 7A_j
+            // (this member of 7A_j was uncommon to the SP5 ring of 7A_i)
+
+            quickSort(&trial[0], 6);
+            quickSort(&trial[6], 3);
+            for (l = 0; l < 10; l++) hc10B[n10B][l] = trial[l];
+
             Cluster_Write_10B();
         }
     }
 }
 
 void Cluster_Write_10B() {
-    // hc10B key: (ordered shell particles, s1, s2, s3 (ordered), s_com)
-    int i;
 
-    for(i=0; i<6; i++) {
+    for(int i=0; i<6; i++) {
         if (s10B[hc10B[n10B][i]] == 'C') s10B[hc10B[n10B][i]] = 'B';
     }
-    for(i=6; i<9; i++) {
+    for(int i=6; i<9; i++) {
         if (s10B[hc10B[n10B][i]] != 'S') s10B[hc10B[n10B][i]] = 'O';
     }
     s10B[hc10B[n10B][9]] = 'S';
